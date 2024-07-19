ea88cde8e319c2195d6550f627e48adc613d6169
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-07-19 15:29:59 +0200

    credential update

--- netlify/functions/handleproxy.js
+++ netlify/functions/handleproxy.js
@@ -7,7 +7,7 @@ exports.handler = async function(event, context) {
   const targetUrl = 'http://90.54.44.103:8081/digitalinput/all/value';
   const username = 'root';
   const password = '00000000';
-  const credentials = Buffer.from(`${username}:${password}`).toString('base64');
+  const credentials = btoa(`${username}:${password}`);
 
   const response = await fetch(targetUrl, {
     method: 'GET',
