437b6ed77c91579e22eb947474eb6103f9081207
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-07-23 14:08:52 +0200

    test on tplink router

--- netlify/functions/handleproxy.js
+++ netlify/functions/handleproxy.js
@@ -1,7 +1,8 @@
 exports.handler = async function(event) {
   const fetch = (await import('node-fetch')).default;
 
-  const proxyUrl = "http://90.54.44.103:8081/digitalinput/all/value";
+  // const proxyUrl = "http://90.54.44.103:8081/digitalinput/all/value"; // livebox home
+  const proxyUrl = "http://10.132.255.190:8081/digitalinput/all/value"; // tplink router 1
   const username = "root";
   const password = "00000000";
   const credentials = Buffer.from(username + ":" + password).toString('base64');
