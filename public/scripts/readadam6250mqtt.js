667a889faf272a2748f89e8201905fe050868288
damoosnz <damien.legrand@bimsolutionscentre.com>
2024-08-22 15:57:47 +1200

    rename mqtt function

--- public/scripts/readadam6250mqtt.js
+++ public/scripts/readadam6250mqtt.js
@@ -1,5 +1,5 @@
 document.getElementById('startBtn').addEventListener('click', async () => {
-    const response = await fetch('https://adam6250.netlify.app/.netlify/functions/mqtt-handler');
+    const response = await fetch('https://adam6250.netlify.app/.netlify/functions/readmqtt');
     const data = await response.json();
     document.getElementById('dataDisplay').innerText = `Received: ${data.message}`;
 });
